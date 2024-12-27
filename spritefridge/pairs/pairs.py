414339e129ffebc928682e8faa0796fa5969c9fa
dmalzl <UaF20091!>
2024-12-27 22:36:28 +0100

    add duplicate stats rename file

--- spritefridge/pairs/pairs.py
+++ spritefridge/pairs/pairs.py
@@ -11,20 +11,35 @@ from collections import defaultdict
 def main(args):
     clusters = defaultdict(set)
     logging.info('reading alignments and constructing clusters')
-    alignments_processed = 0
+    alignments_processed, duplicate_alignments = 0, 0
     for bcs, pos in read_bam(args.bam, args.separator):
+        if pos in clusters[bcs]:
+            duplicate_alignments += 1
+        
         clusters[bcs].add(pos)
         alignments_processed += 1
 
         if not alignments_processed % 1e5:
             logging.info(f'processed {alignments_processed} alignments')
 
-    logging.info('finished cluster construction. writing pairs for all found sizes')
-    stats = clusters_to_pairs(
+    logging.info('finished cluster construction. write duplication stats')
+    alignment_stats = {
+        'unique': alignments_processed - duplicate_alignments,
+        'duplicated': duplicate_alignments
+    }
+    write_stats(
+        alignments_processed, 
+        args.outprefix + '.duplicatestats.tsv'
+    )
+    logging.info('now writing pairs for all found sizes')
+    size_stats = clusters_to_pairs(
         clusters, 
         args.outprefix,
         args.clustersizelow,
         args.clustersizehigh
     )
     logging.info('finished writing pairs. writing stats')
-    write_stats(stats, args.outprefix + '.stats.tsv')
+    write_stats(
+        size_stats, 
+        args.outprefix + '.sizestats.tsv'
+    )
